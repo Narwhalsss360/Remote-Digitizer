93ab1ed27b2ec26e2792cd5a60aa6b79eb4a6d22
Franklyn Vasquez <franklynvasquez24@gmail.com>
2025-04-10 21:02:22 -0400

    Added Constants

---
+++ Common/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public static class Constants
+    {
+        //                                INKER
+        public static readonly int PORT = 46537;
+    }
+}
